df51d63e17bff47ad3d714983e6c2fea989cc6d4
Steve Hunter <steve@smtahunter.com>
2018-01-06 13:12:49 +1100

    Start with Navigation

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+// School Ready - Helping your child get ready for school!
+// Idea by MaryAnn Hunter, maryann@smtahunter.com
+// Coded by Steve Hunter steve@smtahunter.com
 
-export default class HelloWorldApp extends Component {
+import React, { Component } from 'react';
+
+
+
+import {
+  AppRegistry,
+  Text
+} from 'react-native';
+
+// Multiple screen navigation
+// npm install --save react-navigation, see https://facebook.github.io/react-native/docs/navigation.html
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <Text>Hello world!</Text>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
     );
   }
 }
